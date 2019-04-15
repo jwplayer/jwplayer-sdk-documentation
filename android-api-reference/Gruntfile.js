afd7e1a16b897f39143a82ff8edd914dbbf768aa
Monica Parra <monicaparra@MParra-MBP-13.local>
2019-04-15 14:22:56 -0400

    testing api reference landing page

---
+++ android-api-reference/Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function (grunt) {
+
+  'use strict';
+
+  // configure grunt
+  grunt.initConfig({
+    'mkdocs-jwplayer': {
+      build: {},
+      serve: {
+        options: {
+          serve: true
+        }
+      },
+      deploy: {
+        options: {
+          deploy: true
+        }
+      }
+    }
+  });
+
+  // load grunt plugins
+  grunt.loadNpmTasks('grunt-mkdocs-jwplayer');
+
+  // build docs
+  grunt.registerTask('default', [
+    'mkdocs-jwplayer:build'
+  ]);
+
+  // build docs and serve localhost
+  grunt.registerTask('serve', [
+    'mkdocs-jwplayer:serve'
+  ]);
+
+  // build docs and deploy via jenkins
+  grunt.registerTask('deploy', [
+    'mkdocs-jwplayer:deploy'
+  ]);
+
+  // look for and compile custom markdown
+  grunt.registerTask('compile-custom-markdown', function() {
+    grunt.file.recurse('developer-guide', function callback(absPath, rootDir, subDir, filename) {
+      if (filename.substr(filename.length - 4) == 'html') {
+        var html = grunt.file.read(absPath);
+        html = html.replace(/<\w+>\s?\^{3}([\s\S]*?)\^{3}\s?<\/\w+>/g, function(match, cg1, offset, str) {
+          return '<div class="output">' + cg1.trim() + '</div>';
+        });
+        html = html.replace(/<.+>\s?\!{3}([a-z]+)?([\s\S]*?)\!{3}\s?<\/.+>/g, function(match, cg1, cg2, offset, str) {
+          cg1 = cg1 || 'default';
+          return '<div class="alert ' + cg1.trim() + '">' + cg2.trim() + '</div>';
+        });
+        grunt.file.write(absPath, html);
+      }
+    });
+  });
+
+};
---
+++ android-api-reference/docs/index.md
@@ -0,0 +1,29 @@
+# Introduction
+
+<img src="https://img.shields.io/badge/SDK-ANDROID%20v3-0AAC29.svg?logo=apple">
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu fermentum odio. Nulla vel odio imperdiet, laoreet lorem ut, tincidunt tortor. Aliquam a neque ullamcorper, scelerisque sapien quis, ornare libero. Sed lacinia interdum libero at elementum. Donec ultricies varius velit, quis mollis erat pulvinar sit amet.
+
+## Getting Started with the API
+
+Our API supports:
+
+- Configuration
+- Events
+- Events.Listeners
+- Fullscreen
+- Media.Adaptive
+- Media.Ads
+- Media.Audio
+- Media.Captions
+- Media.DRM
+- Media.Meta
+- Media.Playlists
+
+## App Library
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu fermentum odio. Nulla vel odio imperdiet, laoreet lorem ut, tincidunt tortor.
+
+> See our [Javadocs](https://developer.jwplayer.com/sdk/android/reference/)
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eu fermentum odio. Nulla vel odio imperdiet, laoreet lorem ut, tincidunt tortor.
